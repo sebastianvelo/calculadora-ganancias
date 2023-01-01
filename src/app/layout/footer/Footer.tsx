c13c526c869c4270b0a3c0d17b58ebe51079e8e6
Sebastian <sebastian.velo@mercadolibre.com>
2023-01-01 17:43:37 -0300

    update january 2023

--- src/app/layout/footer/Footer.tsx
+++ src/app/layout/footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: FunctionComponent = () => (
             <a target="_blank" href="https://github.com/sebastianvelo/calculadora-ganancias"><img className="w-8 h-8 filter invert" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Github Repo" /></a>
         </div>
         <div className="flex flex-col md:flex-row md:space-x-4 text-xs divide-y-2 space-y-2 md:space-y-0 md:divide-y-0 md:divide-x-2 divide-primary">
-            <p className="pt-2">Calculadora de Impuesto a las Ganancias 2022 </p>
+            <p className="pt-2">Calculadora de Impuesto a las Ganancias 2023 </p>
             <a className="pt-2 md:px-2" target="_blank" href="https://www.linkedin.com/in/sebastian-velo/">Hecho con ♥ por Sebastián Velo</a>
         </div>
     </footer>
--- src/app/layout/navigation/Navigation.tsx
+++ src/app/layout/navigation/Navigation.tsx
@@ -6,8 +6,8 @@ interface NavigationProps {
 
 const Navigation: FunctionComponent<NavigationProps> = () => (
     <nav className="bg-gradient-to-b from-gray-800 to-black text-white px-2 py-4 flex flex-col md:flex-row justify-between">
-        <h1 className="md:text-2xl font-black md:text-center">Calculadora de ganancias 2022</h1>
-        <p className="pt-2 px-2">Actualizada al 1 de noviembre de 2022</p>
+        <h1 className="md:text-2xl font-black md:text-center">Calculadora de ganancias 2023</h1>
+        <p className="pt-2 px-2">Actualizada al 1 de enero de 2023</p>
     </nav>
 )
 
